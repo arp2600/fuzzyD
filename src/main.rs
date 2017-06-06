88dd2d0186019816b9404fd235ce9747f96d1fd7
Sam Kellett <samkellett@gmail.com>
2017-06-06 21:43:29 +0100

    Changed main to search cwd for all files that match.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
+extern crate find;
 extern crate fuzz;
 
-fn main() {
-    // Keep taking user input
+use std::env;
+use std::io;
 
-    let haystack = "the quick brown fox jumped over the lazy dog.";
-    println!("Calculate fuzz-score from the string \"{}\":", haystack);
+fn main() {
+    use io::Write;
+
+    print!("Enter search term: ");
+    io::stdout().flush().unwrap();
 
     let mut needle = String::new();
+    io::stdin().read_line(&mut needle).expect("Error reading input.");
+    let needle = needle.trim();
 
-    loop {
-        print!("> ");
+    let current_working_directory = env::current_dir().unwrap();
+    let finder = find::find(current_working_directory)
+        .filter(|ref p| fuzz::substrings(&needle, p.to_str().unwrap()) != fuzz::Score(0));
 
-        needle.clear();
-        match std::io::stdin().read_line(&mut needle) {
-            Ok(_) => {
-                let score = fuzz::substrings(&needle.trim(), &haystack);
-                println!("{:?}", score);
-            }
-            Err(err) => println!("Error: {}", err),
-        }
+    for path in finder {
+        println!("Matched: {:?}", path);
     }
 }
 
